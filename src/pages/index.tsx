78f705433a21dca0eb937f03bd550b29862e11f9
Leo Adamek <leo.adamek@mrzen.com>
2023-03-03 17:12:12 +0000

    Fix broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/product-provider/tutorial-build-a-product-provider/introduction">
+            to="/docs/platform/product-provider/tutorial-build-a-product-provider/introduction">
             Build a Product Provider - 30min ⏱️
           </Link>
         </div>
