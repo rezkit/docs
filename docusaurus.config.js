763998fcd7b611dc310a63516749f2975abe9d5f
Tom Hood <tom.hood@mrzen.com>
2024-04-03 14:34:47 +0100

    RK-737 Roll-back state and sandbox testing.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/custom.scss'),
         },
       }),
     ],
@@ -66,6 +66,11 @@ const config = {
             position: 'left',
             label: 'Tours',
             sidebarId: 'tours'
+          },
+          {
+            position: 'right',
+            label: 'Contact',
+            href: '/contact'
           },
           {
             href: 'https://github.com/mrzen/rezkit-docs',
